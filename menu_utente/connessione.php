e6a0d6f61564062eabeeae9d5f79a870f592a6ae
esborni.mattia.07@itisdalmine.edu.it <esborni.mattia.07@itisdalmine.edu.it>
2026-01-25 10:57:07 +0100

    modifica ai file conneessione.php per farlo funzionare hostato su infinityfree

--- menu_utente/connessione.php
+++ menu_utente/connessione.php
@@ -1,9 +1,17 @@
 <?php
+    /*
     // parametri connessione al database
     $conn_server = 'localhost';
     $conn_db = 'pizzeria';
     $conn_usr = 'root';
     $conn_psw = '';
+
+    $conn_str = 'mysql:host=' . $conn_server . ';dbname=' . $conn_db;
+    */
+    $conn_server = 'sql211.infinityfree.com';
+    $conn_db = 'if0_40977072_pizzeria';
+    $conn_usr = 'if0_40977072';
+    $conn_psw = 'IlCentauro2';
 
     $conn_str = 'mysql:host=' . $conn_server . ';dbname=' . $conn_db;
 ?>
